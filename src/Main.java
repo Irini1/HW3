c06243866652aa70e426dd2953dc52d4a95bcabf
Irini <learningnewproggram>
2022-06-13 11:14:38 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int iOS = 0;
+        if (iOS >= 0);{
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        int clientAndroid = 1;
+        if (clientAndroid >=1); {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
+        int clientDeviceYear = 2015;
+        if (clientDeviceYear <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        }
+        if (clientDeviceYear >=16) {
+            System.out.println("Установите приложение банка");
+        }
+
     }
 }
