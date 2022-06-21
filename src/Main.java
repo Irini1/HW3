50e9c1958127d2cf10c1481f5f15e6f4f226d1e1
Irini <learningnewproggram>
2022-06-21 11:25:03 +0300

    Merge remote-tracking branch 'origin/dz3' into dz3

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int iOS = 0;
-        if (iOS >= 0);{
+        if (iOS >= 0) ;
+        {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
         int clientAndroid = 1;
-        if (clientAndroid >=1); {
+        if (clientAndroid >= 1) ;
+        {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
@@ -14,16 +16,22 @@ public class Main {
         if (clientDeviceYear <= 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
-        if (clientDeviceYear >=16) {
+        if (clientDeviceYear >= 16) {
             System.out.println("Установите приложение банка");
         }
 
         int year = 2022;
-        if (year %4 == 0 && year %100 != 0 || year%400 == 0){
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("Год является високосным");
-        }
-        else
+        } else
             System.out.println("Не високосный");
+
+        int distance = 20;
+        int deliveryDictance = 95;
+        if (distance == 20 && deliveryDictance < 95)
+            System.out.println("Доставка карты в этот же день");
+        if (distance >= 20 && deliveryDictance < 95)
+            System.out.println("Доставка карты на следующий день");
 
     }
 }
