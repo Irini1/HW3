4447138f9a64448aec0f818226b79d4b4f43d14c
Irini <learningnewproggram>
2022-06-21 15:07:23 +0300

    Merge remote-tracking branch 'origin/dz3' into dz3

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        int iOS = 0;
-        if (iOS >= 0) ;
+        int iOS = 1;
+        if (iOS <= 0) ;
         {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
@@ -19,19 +19,53 @@ public class Main {
         if (clientDeviceYear >= 16) {
             System.out.println("Установите приложение банка");
         }
-
         int year = 2022;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("Год является високосным");
         } else
-            System.out.println("Не високосный");
+            System.out.println("2022 Не високосный");
 
-        int distance = 20;
+        int deliveryDays = 1;
+
         int deliveryDictance = 95;
-        if (distance == 20 && deliveryDictance < 95)
-            System.out.println("Доставка карты в этот же день");
-        if (distance >= 20 && deliveryDictance < 95)
-            System.out.println("Доставка карты на следующий день");
 
-    }
-}
+        if (deliveryDictance > 20) {
+            deliveryDays++;
+        }
+
+        if (deliveryDictance > 60) {
+            deliveryDays++;
+        }
+        System.out.println("Потребуется дней " + deliveryDays);
+
+        {
+            task5();
+            {
+                int mounthNumber = 1;
+                switch (mounthNumber) {
+                    case 12:
+                    case 1:
+                    case 2:
+                        System.out.println("Зима");
+                        break;
+                    case 3:
+                    case 4:
+                    case 5:
+                        System.out.println("Весна");
+                        break;
+                    case 6:
+                    case 7:
+                    case 8:
+                        System.out.println("Лето");
+                        break;
+                    case 9:
+                    case 10:
+                    case 11:
+                        System.out.println("Осень");
+                        break;
+                    default:
+                        System.out.println("Некорректный месяц: " + mounthNumber);
+                }
+
+            }
+        }
